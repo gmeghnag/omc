1a4596a300cbaf647927d0c0592ed372b7119561
cpassare <cpassare@redhat.com>
2022-01-09 19:39:46 +0100

    recreated the file starting from proxy.go

--- cmd/get/openshift/operator/authentications.go
+++ cmd/get/openshift/operator/authentications.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Bram Verschueren <bverschueren@redhat.com>
+Copyright © 2021 Christian Passarelli <cpassare@redhat.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -21,82 +21,101 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/gmeghnag/omc/cmd/helpers"
 	"github.com/gmeghnag/omc/vars"
 
 	operatorv1 "github.com/openshift/api/operator/v1"
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/util/duration"
 
 	"sigs.k8s.io/yaml"
 )
 
-type authenticationItems struct {
-	ApiVersion string                      `json:"apiVersion"`
-	Items      []operatorv1.Authentication `json:"items"`
-}
+func getAuthentications(currentContextPath string, namespace string, resourceName string, allNamespacesFlag bool, outputFlag string, showLabels bool, jsonPathTemplate string) bool {
 
-func getAuthentication(currentContextPath string, namespace string, resourceName string, allNamespacesFlag bool, outputFlag string, showLabels bool, jsonPathTemplate string) bool {
+	authenticationsFolderPath := currentContextPath + "/cluster-scoped-resources/operator.openshift.io/authentications/"
+	_authentications, _ := ioutil.ReadDir(authenticationsFolderPath)
 
-	// There is only one authentication per cluster, therefore the must gather
-	// only contains a single authentication rather than a list. Do not take
-	// this file as an example because most of what you see is exceptional.
+	_headers := []string{"name", "age"}
+	var data [][]string
 
-	if resourceName != "" && resourceName != "cluster" {
-		fmt.Println("omc only supports the \"cluster\" authentication. Try omc get authentication or omc get authentication cluster")
-		os.Exit(1)
-	}
-
-	authenticationYamlPath := currentContextPath + "/cluster-scoped-resources/operator.openshift.io/authentications/cluster.yaml"
-
-	_file, _ := ioutil.ReadFile(authenticationYamlPath)
-
-	authentication := operatorv1.Authentication{}
-	if err := yaml.Unmarshal([]byte(_file), &authentication); err != nil {
-		fmt.Println("Error when trying to unmarshal file: " + authenticationYamlPath)
-		os.Exit(1)
-	}
-
-	if outputFlag == "" || outputFlag == "wide" {
-		headers := []string{"NAME", "AGE"}
-		authenticationName := authentication.Name
-		age := ""
-		//if authentication.CreationTimestamp != nil {
-		age = duration.ShortHumanDuration(time.Now().Sub(authentication.CreationTimestamp))
-		//age = authentication.CreationTimestamp.String()
-		//}
-		authenticationAGE := age
-		labels := helpers.ExtractLabels(authentication.GetLabels())
-
-		cn := []string{authenticationName, authenticationAGE}
-
-		if showLabels {
-			headers = append(headers, "labels")
-			cn = append(cn, labels)
+	_AuthenticationsList := operatorv1.AuthenticationList{}
+	for _, f := range _authentications {
+		authenticationsYamlPath := authenticationsFolderPath + f.Name()
+		_file, _ := ioutil.ReadFile(authenticationsYamlPath)
+		Authentication := operatorv1.Authentication{}
+		if err := yaml.Unmarshal([]byte(_file), &Authentication); err != nil {
+			fmt.Println("Error when trying to unmarshal file: " + authenticationsYamlPath)
+			os.Exit(1)
 		}
 
-		data := [][]string{cn}
-		helpers.PrintTable(headers, data)
+		if resourceName != "" && resourceName != Authentication.Name {
+			continue
+		}
+
+		if outputFlag == "yaml" {
+			_AuthenticationsList.Items = append(_AuthenticationsList.Items, Authentication)
+			continue
+		}
+
+		if outputFlag == "json" {
+			_AuthenticationsList.Items = append(_AuthenticationsList.Items, Authentication)
+			continue
+		}
+
+		if strings.HasPrefix(outputFlag, "jsonpath=") {
+			_AuthenticationsList.Items = append(_AuthenticationsList.Items, Authentication)
+			continue
+		}
+		//Name
+		authenticationName := Authentication.Name
+		age := helpers.GetAge(authenticationsYamlPath, Authentication.GetCreationTimestamp())
+
+		labels := helpers.ExtractLabels(Authentication.GetLabels())
+		_list := []string{authenticationName, age}
+		data = helpers.GetData(data, true, showLabels, labels, outputFlag, 2, _list)
 	}
 
+	var headers []string
+	if outputFlag == "" {
+		headers = _headers[0:2] // -A
+		if showLabels {
+			headers = append(headers, "labels")
+		}
+		helpers.PrintTable(headers, data)
+		return false
+
+	}
+	if outputFlag == "wide" {
+		headers = _headers // -A -o wide
+		if showLabels {
+			headers = append(headers, "labels")
+		}
+		helpers.PrintTable(headers, data)
+		return false
+	}
+	var resource interface{}
+	if resourceName != "" {
+		resource = _AuthenticationsList.Items[0]
+	} else {
+		resource = _AuthenticationsList
+	}
 	if outputFlag == "yaml" {
-		y, _ := yaml.Marshal(authentication)
+		y, _ := yaml.Marshal(resource)
 		fmt.Println(string(y))
 	}
 	if outputFlag == "json" {
-		j, _ := json.MarshalIndent(authentication, "", "  ")
+		j, _ := json.MarshalIndent(resource, "", "  ")
 		fmt.Println(string(j))
 	}
 	if strings.HasPrefix(outputFlag, "jsonpath=") {
-		helpers.ExecuteJsonPath(authentication, jsonPathTemplate)
+		helpers.ExecuteJsonPath(resource, jsonPathTemplate)
 	}
 	return false
 }
 
 var Authentication = &cobra.Command{
-	Use:     "authentication.operator",
+	Use:     "authentications.operator",
 	Aliases: []string{"authentication.operator", "authentication.operator.openshift.io", "authentications.operator", "authentications.operator.openshift.io"},
 	Hidden:  true,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -105,6 +124,6 @@ var Authentication = &cobra.Command{
 			resourceName = args[0]
 		}
 		jsonPathTemplate := helpers.GetJsonTemplate(vars.OutputStringVar)
-		getAuthentication(vars.MustGatherRootPath, vars.Namespace, resourceName, vars.AllNamespaceBoolVar, vars.OutputStringVar, vars.ShowLabelsBoolVar, jsonPathTemplate)
+		getAuthentications(vars.MustGatherRootPath, vars.Namespace, resourceName, vars.AllNamespaceBoolVar, vars.OutputStringVar, vars.ShowLabelsBoolVar, jsonPathTemplate)
 	},
 }
