a3cfb76b85d5f9d0c93424309c4d7142183fb435
gmeghnag <gmeghnag@redhat.com>
2022-08-18 17:29:29 +0200

    Implemented #56

---
+++ cmd/get/generic-resource.go
@@ -0,0 +1,273 @@
+package get
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"bytes"
+
+	"github.com/gmeghnag/omc/cmd/helpers"
+	"github.com/gmeghnag/omc/cmd/uget"
+	"github.com/gmeghnag/omc/vars"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/util/jsonpath"
+	"sigs.k8s.io/yaml"
+)
+
+type AdditionalPrinterColumn struct {
+	Description string `json:"id"`
+	JsonPath    string `json:"path"`
+	Name        string `json:"current"`
+	Type        string `json:"project"`
+}
+
+var data [][]string
+var headers []string
+var returnObjects = uget.UnstrctList{ApiVersion: "v1", Kind: "List"}
+
+func getGenericResourceFromCRD(crdName string, objectNames []string) bool {
+	var crd *apiextensionsv1.CustomResourceDefinition
+	crdsPath := vars.MustGatherRootPath + "/cluster-scoped-resources/apiextensions.k8s.io/customresourcedefinitions/"
+	BoolReturn := false
+	if strings.HasSuffix(crdName, ".config") {
+		crdName = strings.Replace(crdName, ".config", ".config.openshift.io", -1)
+	}
+	_, err := Exists(crdsPath)
+	if err != nil {
+		fmt.Printf(err.Error())
+		os.Exit(1)
+	}
+	crds, _ := ioutil.ReadDir(crdsPath)
+	for _, f := range crds {
+		crdYamlPath := crdsPath + f.Name()
+		crdByte, _ := ioutil.ReadFile(crdYamlPath)
+		_crd := &apiextensionsv1.CustomResourceDefinition{}
+		if err := yaml.Unmarshal([]byte(crdByte), &_crd); err != nil {
+			fmt.Println("Error when trying to unmarshal file ")
+			os.Exit(1)
+		}
+		if strings.ToLower(_crd.Name) == strings.ToLower(crdName) || strings.ToLower(_crd.Spec.Names.Plural) == strings.ToLower(crdName) || strings.ToLower(_crd.Spec.Names.Singular) == strings.ToLower(crdName) || helpers.StringInSlice(crdName, _crd.Spec.Names.ShortNames) || _crd.Spec.Names.Singular+"."+_crd.Spec.Group == strings.ToLower(crdName) {
+			BoolReturn = true
+			crd = _crd
+			break
+		}
+	}
+	if BoolReturn == false {
+		return false
+	}
+	if vars.AllNamespaceBoolVar && crd.Spec.Scope != "Cluster" {
+		headers = append(headers, "Namespace")
+	}
+	headers = append(headers, "Name")
+	if crd.Spec.Versions[0].AdditionalPrinterColumns == nil {
+		headers = append(headers, "Age")
+	}
+	if crd.Spec.Scope == "Cluster" {
+		resourcesPath := vars.MustGatherRootPath + "/cluster-scoped-resources/" + crd.Spec.Group + "/" + crd.Spec.Names.Plural + "/"
+		bole, _ := Exists(resourcesPath)
+		if !bole {
+			resourcesPath = vars.MustGatherRootPath + "/cluster-scoped-resources/" + crd.Spec.Group + "/"
+		}
+		gatherObjects(resourcesPath, crd, objectNames)
+	} else {
+		if vars.AllNamespaceBoolVar {
+			namespacesPath := vars.MustGatherRootPath + "/namespaces/"
+			namespaces, _ := ioutil.ReadDir(namespacesPath)
+			for _, ns := range namespaces {
+				resourcesPath := vars.MustGatherRootPath + "/namespaces/" + ns.Name() + "/" + crd.Spec.Group + "/" + crd.Spec.Names.Plural + "/"
+				bole, _ := Exists(resourcesPath)
+				if !bole {
+					resourcesPath = vars.MustGatherRootPath + "/namespaces/" + ns.Name() + "/" + crd.Spec.Group + "/"
+				}
+				gatherObjects(resourcesPath, crd, objectNames)
+			}
+		} else {
+			resourcesPath := vars.MustGatherRootPath + "/namespaces/" + vars.Namespace + "/" + crd.Spec.Group + "/" + crd.Spec.Names.Plural + "/"
+			bole, _ := Exists(resourcesPath)
+			if !bole {
+				resourcesPath = vars.MustGatherRootPath + "/namespaces/" + vars.Namespace + "/" + crd.Spec.Group + "/"
+			}
+			gatherObjects(resourcesPath, crd, objectNames)
+		}
+	}
+	if !BoolReturn {
+		return BoolReturn
+	}
+	if vars.OutputStringVar == "" {
+		if len(data) == 0 {
+			fmt.Println("No resources found.")
+			os.Exit(1)
+		} else {
+			if vars.ShowLabelsBoolVar {
+				headers = append(headers, "labels")
+			}
+			helpers.PrintTable(headers, data)
+		}
+	} else {
+		if len(returnObjects.Items) == 0 {
+			fmt.Println("No resources found.")
+			os.Exit(1)
+		}
+		if vars.OutputStringVar == "json" {
+			if len(returnObjects.Items) == 1 {
+				j, _ := json.MarshalIndent(returnObjects.Items[0].Object, "", "  ")
+				fmt.Println(string(j))
+			} else {
+				j, _ := json.MarshalIndent(returnObjects, "", "  ")
+				fmt.Println(string(j))
+			}
+		} else if vars.OutputStringVar == "yaml" {
+			if len(returnObjects.Items) == 1 {
+				y, _ := yaml.Marshal(returnObjects.Items[0].Object)
+				fmt.Println(string(y))
+			} else {
+				y, _ := yaml.Marshal(returnObjects)
+				fmt.Println(string(y))
+			}
+		} else if strings.HasPrefix(vars.OutputStringVar, "jsonpath=") {
+			jsonPathTemplate := helpers.GetJsonTemplate(vars.OutputStringVar)
+			if len(returnObjects.Items) == 1 {
+				helpers.ExecuteJsonPath(returnObjects.Items[0].Object, jsonPathTemplate)
+			} else {
+				helpers.ExecuteJsonPath(returnObjects, jsonPathTemplate)
+			}
+		} else if vars.OutputStringVar == "name" {
+			for _, obj := range returnObjects.Items {
+				fmt.Println(strings.ToLower(obj.GetKind()) + "/" + obj.GetName())
+			}
+		}
+	}
+	return BoolReturn
+}
+
+func gatherObjects(resourcePath string, crd *apiextensionsv1.CustomResourceDefinition, objectNames []string) {
+	_, err := Exists(resourcePath)
+	if err != nil {
+		fmt.Printf(err.Error())
+		os.Exit(1)
+	}
+	var JSONPaths []apiextensionsv1.CustomResourceColumnDefinition
+	for _, column := range crd.Spec.Versions[0].AdditionalPrinterColumns {
+		headers = append(headers, column.Name)
+		JSONPaths = append(JSONPaths, column)
+	}
+	resources, _ := ioutil.ReadDir(resourcePath)
+	for _, f := range resources {
+		if f.IsDir() {
+			continue
+		}
+		resourceYamlPath := resourcePath + f.Name()
+		resourceByte, _ := ioutil.ReadFile(resourceYamlPath)
+		unstruct := &unstructured.Unstructured{}
+		if err := yaml.Unmarshal([]byte(resourceByte), &unstruct); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		if unstruct.IsList() {
+			unstructList := &unstructured.UnstructuredList{}
+			err := yaml.Unmarshal([]byte(resourceByte), &unstructList)
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+			for _, resource := range unstructList.Items {
+				var resourceData []string
+				resourceName := resource.GetName()
+				if resource.GetKind() == crd.Spec.Names.Kind {
+					labels := helpers.ExtractLabels(unstruct.GetLabels())
+					if helpers.MatchLabels(labels, vars.LabelSelectorStringVar) && (len(objectNames) == 0 || helpers.StringInSlice(resourceName, objectNames)) {
+						if vars.OutputStringVar == "" {
+							if vars.AllNamespaceBoolVar && crd.Spec.Scope != "Cluster" {
+								resourceData = append(resourceData, resource.GetNamespace())
+							}
+							resourceData = append(resourceData, resourceName)
+							if crd.Spec.Versions[0].AdditionalPrinterColumns == nil {
+								unstruct := &unstructured.Unstructured{}
+								if err := yaml.Unmarshal([]byte(resourceByte), &unstruct); err != nil {
+									fmt.Println("File:", resourcePath, " does not contain a valid k8s object,", err.Error())
+									os.Exit(1)
+								}
+								v := helpers.GetAge(resourceYamlPath, unstruct.GetCreationTimestamp())
+								resourceData = append(resourceData, v)
+							} else {
+								for _, column := range crd.Spec.Versions[0].AdditionalPrinterColumns {
+									v := getFromJsonPath(resource.Object, "{"+column.JSONPath+"}")
+									if column.Type == "date" {
+										v = helpers.GetAge(resourceYamlPath, unstruct.GetCreationTimestamp())
+									}
+									resourceData = append(resourceData, v)
+								}
+							}
+							if vars.ShowLabelsBoolVar {
+								resourceData = append(resourceData, labels)
+							}
+						} else {
+							returnObjects.Items = append(returnObjects.Items, *unstruct)
+						}
+					}
+				}
+				data = append(data, resourceData)
+			}
+		} else {
+			var resourceData []string
+			if unstruct.GetKind() == crd.Spec.Names.Kind {
+				resourceName := unstruct.GetName()
+				labels := helpers.ExtractLabels(unstruct.GetLabels())
+				if helpers.MatchLabels(labels, vars.LabelSelectorStringVar) && (len(objectNames) == 0 || helpers.StringInSlice(resourceName, objectNames)) {
+					if vars.OutputStringVar == "" {
+						if vars.AllNamespaceBoolVar && crd.Spec.Scope != "Cluster" {
+							resourceData = append(resourceData, unstruct.GetNamespace())
+						}
+						resourceData = append(resourceData, resourceName)
+						for _, jpath := range JSONPaths {
+							v := getFromJsonPath(unstruct.Object, "{"+jpath.JSONPath+"}")
+							if jpath.Type == "date" {
+								v = helpers.GetAge(resourceYamlPath, unstruct.GetCreationTimestamp())
+							}
+							resourceData = append(resourceData, v)
+						}
+						if crd.Spec.Versions[0].AdditionalPrinterColumns == nil {
+							age := helpers.GetAge(resourceYamlPath, unstruct.GetCreationTimestamp())
+							resourceData = append(resourceData, age)
+						}
+						if vars.ShowLabelsBoolVar {
+							resourceData = append(resourceData, labels)
+						}
+						data = append(data, resourceData)
+					} else {
+						returnObjects.Items = append(returnObjects.Items, *unstruct)
+					}
+				}
+			}
+		}
+	}
+}
+
+func getFromJsonPath(data interface{}, jsonPathTemplate string) string {
+	buf := new(bytes.Buffer)
+	jPath := jsonpath.New("out")
+	jPath.AllowMissingKeys(false)
+	jPath.EnableJSONOutput(false)
+	err := jPath.Parse(jsonPathTemplate)
+	if err != nil {
+		fmt.Println("error: error parsing jsonpath " + jsonPathTemplate + ", " + err.Error())
+		os.Exit(1)
+	}
+	jPath.Execute(buf, data)
+	return buf.String()
+}
+
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
--- cmd/get/get.go
+++ cmd/get/get.go
@@ -52,6 +52,8 @@ import (
 
 var outputStringVar string
 var allNamespaceBoolVar, showLabelsBoolVar bool
+var emptyslice []string
+var resourcesAndObjects [][]string
 
 var GetCmd = &cobra.Command{
 	Use: "get",
@@ -61,36 +63,108 @@ var GetCmd = &cobra.Command{
 			os.Exit(0)
 		}
 		// support (not completed) for comma separated resources
-		if len(args) == 1 && strings.Contains(args[0], ",") {
-			commaSeparatedResources := strings.TrimSuffix(args[0], ",")
-			commaSeparatedResources = strings.TrimPrefix(commaSeparatedResources, ",")
-			resources := strings.Split(commaSeparatedResources, ",")
-			for _, r := range resources {
-				c, _, err := cmd.Find([]string{r})
+		if strings.Contains(args[0], "/") {
+			for _, resourceAndObjectArg := range args {
+				resourceAndObjectArg = strings.ToLower(resourceAndObjectArg)
+				if len(strings.Split(resourceAndObjectArg, "/")) != 2 {
+					fmt.Println("error: there is no need to specify a resource type as a separate argument when passing arguments in resource/name form (e.g. 'oc get resource/<resource_name>' instead of 'oc get resource resource/<resource_name>'")
+					os.Exit(1)
+				}
+				seg := strings.Split(resourceAndObjectArg, "/")
+				crdAlias, objectName := seg[0], seg[1]
+				aliasAlreadyPresentInResourcesAndObjects := false
+				for i, resourceAndObject := range resourcesAndObjects {
+					if crdAlias == resourceAndObject[0] {
+						aliasAlreadyPresentInResourcesAndObjects = true
+						resourcesAndObjects[i] = append(resourcesAndObjects[i], objectName)
+						break
+					}
+				}
+				if !aliasAlreadyPresentInResourcesAndObjects {
+					resourcesAndObjects = append(resourcesAndObjects, []string{crdAlias, objectName})
+				}
+
+			}
+			for _, resourceAndObject := range resourcesAndObjects {
+				c, _, err := cmd.Find([]string{resourceAndObject[0]})
 				if err != nil {
 					fmt.Println("err", err.Error())
+					os.Exit(1)
 				}
-				os.Args = append([]string{os.Args[0], "get", r}, os.Args[3:]...)
-				c.Execute()
-				fmt.Println("")
+				if c.Use != "get" {
+					os.Args = append([]string{os.Args[0], "get", resourceAndObject[0]}, resourceAndObject[1:]...)
+					c.Execute()
+				} else {
+					isValidResource := getGenericResourceFromCRD(resourceAndObject[0], resourceAndObject[1:])
+					if !isValidResource {
+						fmt.Println("Invalid object type:", resourceAndObject[0])
+						os.Exit(1)
+					}
+				}
 			}
-			os.Exit(0)
 		}
-		// object not found
-		fmt.Println("Invalid object type:", args[0])
-		os.Exit(1)
+		if len(args) == 1 {
+			if strings.Contains(args[0], ",") {
+				commaSeparatedResources := strings.TrimSuffix(args[0], ",")
+				commaSeparatedResources = strings.TrimPrefix(commaSeparatedResources, ",")
+				crdAliases := strings.Split(commaSeparatedResources, ",")
+				for _, crdAlias := range crdAliases {
+					c, _, err := cmd.Find([]string{crdAlias})
+					if err != nil {
+						fmt.Println("err", err.Error())
+						os.Exit(1)
+					}
+					if c.Use != "get" {
+						os.Args = append([]string{os.Args[0], "get", crdAlias}, "")
+						c.Execute()
+					} else {
+
+						isValidResource := getGenericResourceFromCRD(crdAlias, emptyslice)
+						if !isValidResource {
+							fmt.Println("Invalid object type:", crdAlias)
+							os.Exit(1)
+						}
+					}
+				}
+				fmt.Println("")
+			} else {
+				c, _, err := cmd.Find([]string{args[0]})
+				if err != nil {
+					fmt.Println("err", err.Error())
+					os.Exit(1)
+				}
+				if c.Use != "get" {
+					os.Args = append([]string{os.Args[0], "get", args[0]}, "")
+					c.Execute()
+				} else {
+					isValidResource := getGenericResourceFromCRD(args[0], emptyslice)
+					if !isValidResource {
+						fmt.Println("Invalid object type:", args[0])
+						os.Exit(1)
+					}
+				}
+			}
+		} else if len(args) > 1 {
+			c, _, err := cmd.Find([]string{args[0]})
+			if err != nil {
+				fmt.Println("err", err.Error())
+				os.Exit(1)
+			}
+			if c.Use != "get" {
+				os.Args = append([]string{os.Args[0], "get", args[0]}, args[1:]...)
+				c.Execute()
+			} else {
+				isValidResource := getGenericResourceFromCRD(args[0], args[1:])
+				if !isValidResource {
+					fmt.Println("Invalid object type:", args[0])
+					os.Exit(1)
+				}
+			}
+		}
 	},
 }
 
 func init() {
-	if len(os.Args) > 2 && os.Args[1] == "get" {
-		os.Args[2] = strings.ToLower(os.Args[2])
-		if strings.Contains(os.Args[2], "/") {
-			seg := strings.Split(os.Args[2], "/")
-			resource, name := seg[0], seg[1]
-			os.Args = append([]string{os.Args[0], "get", resource, name}, os.Args[3:]...)
-		}
-	}
 	GetCmd.PersistentFlags().BoolVarP(&vars.AllNamespaceBoolVar, "all-namespaces", "A", false, "If present, list the requested object(s) across all namespaces.")
 	GetCmd.PersistentFlags().BoolVarP(&vars.ShowLabelsBoolVar, "show-labels", "", false, "When printing, show all labels as the last column (default hide labels column)")
 	GetCmd.PersistentFlags().StringVarP(&vars.OutputStringVar, "output", "o", "", "Output format. One of: json|yaml|wide|jsonpath=...")
