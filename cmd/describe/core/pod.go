309cb476adcb12092b56692610952dd3bbda7510
Gabriel Meghnagi <gmeghnag@redhat.com>
2025-12-30 13:42:57 +0100

    Fix #206

--- cmd/describe/core/pod.go
+++ cmd/describe/core/pod.go
@@ -26,25 +26,55 @@ import (
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/klog/v2"
 	"k8s.io/kubectl/pkg/describe"
 	"sigs.k8s.io/yaml"
 )
 
 func describePod(currentContextPath string, defaultConfigNamespace string, args []string) {
 	podResources := currentContextPath + "/namespaces/" + defaultConfigNamespace + "/core/pods.yaml"
-	PodList := corev1.PodList{}
-	_file := helpers.ReadYaml(podResources)
-	if err := yaml.Unmarshal(_file, &PodList); err != nil {
-		fmt.Fprintln(os.Stderr, "Error when trying to unmarshal file "+podResources)
-		os.Exit(1)
-	}
-	for _, pod := range PodList.Items {
-		if len(args) == 0 || (len(args) > 0 && helpers.StringInSlice(pod.GetName(), args)) {
-			fake := fake.NewSimpleClientset(&pod)
-			c := &types.DescribeClient{Namespace: defaultConfigNamespace, Interface: fake}
-			d := describe.PodDescriber{c}
-			out, _ := d.Describe(defaultConfigNamespace, pod.GetName(), describe.DescriberSettings{ShowEvents: false})
-			fmt.Printf("%s", out)
+	_file, err := os.ReadFile(podResources)
+	if err == nil {
+		PodList := corev1.PodList{}
+		if err := yaml.Unmarshal(_file, &PodList); err != nil {
+			fmt.Fprintln(os.Stderr, "Error when trying to unmarshal file "+podResources)
+			os.Exit(1)
+		}
+		for _, pod := range PodList.Items {
+			if len(args) == 0 || (len(args) > 0 && helpers.StringInSlice(pod.GetName(), args)) {
+				fake := fake.NewSimpleClientset(&pod)
+				c := &types.DescribeClient{Namespace: defaultConfigNamespace, Interface: fake}
+				d := describe.PodDescriber{c}
+				out, _ := d.Describe(defaultConfigNamespace, pod.GetName(), describe.DescriberSettings{ShowEvents: false})
+				fmt.Printf("%s", out)
+			}
+		}
+	} else {
+		podsDir := fmt.Sprintf("%s/namespaces/%s/pods", vars.MustGatherRootPath, defaultConfigNamespace)
+		pods, rErr := os.ReadDir(podsDir)
+		if rErr != nil {
+			klog.V(3).ErrorS(err, "Failed to read resources:")
+		}
+		for _, pod := range pods {
+			podName := pod.Name()
+			podPath := fmt.Sprintf("%s/%s/%s.yaml", podsDir, podName, podName)
+			_file, err := os.ReadFile(podPath)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error reading %s: %s\n", podPath, err)
+				os.Exit(1)
+			}
+			var podItem corev1.Pod
+			if err := yaml.Unmarshal(_file, &podItem); err != nil {
+				fmt.Fprintln(os.Stderr, "Error when trying to unmarshal file "+podPath)
+				os.Exit(1)
+			}
+			if len(args) == 0 || (len(args) > 0 && helpers.StringInSlice(podItem.GetName(), args)) {
+				fake := fake.NewSimpleClientset(&podItem)
+				c := &types.DescribeClient{Namespace: defaultConfigNamespace, Interface: fake}
+				d := describe.PodDescriber{c}
+				out, _ := d.Describe(defaultConfigNamespace, podItem.GetName(), describe.DescriberSettings{ShowEvents: false})
+				fmt.Printf("%s", out)
+			}
 		}
 	}
 }
